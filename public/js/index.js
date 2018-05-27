3c1fc49a001ee76e109509d53d77e8efd124f469
webslinger74 <30727673+webslinger74@users.noreply.github.com>
2018-05-28 00:33:40 +0100

    git commit -m "jquery form"

--- public/js/index.js
+++ public/js/index.js
@@ -15,4 +15,18 @@ socket.on('disconnect', function(){
 socket.on('newMessage', function(message){
     console.log('new message received from server!', message);
 });
-
+socket.emit('createMessage', {
+    from: 'Steven',
+    text: 'Hi'
+}, function(data){
+    console.log('got it', data);
+});
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    },function(message){
+        console.log(message);
+    });
+});

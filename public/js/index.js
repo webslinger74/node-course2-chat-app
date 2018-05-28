a8fb4a400d5efb03c6aa0bd2dcaecdaec30f56c5
webslinger74 <30727673+webslinger74@users.noreply.github.com>
2018-05-28 01:00:51 +0100

    add form with jquery

--- public/js/index.js
+++ public/js/index.js
@@ -14,13 +14,13 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function(message){
     console.log('new message received from server!', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    
+    jQuery('#messages').append(li);
 });
-socket.emit('createMessage', {
-    from: 'Steven',
-    text: 'Hi'
-}, function(data){
-    console.log('got it', data);
-});
+
+
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
     socket.emit('createMessage', {

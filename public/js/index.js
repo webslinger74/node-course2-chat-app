a5c8fe7b18c23a67f68c6a20d7f55eccb9ab1406
webslinger74 <30727673+webslinger74@users.noreply.github.com>
2018-05-29 00:25:05 +0100

    styling page

--- public/js/index.js
+++ public/js/index.js
@@ -33,11 +33,13 @@ socket.on('newLocationMessage', function(location){
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
-    },function(message){
-        console.log(message);
+        text: messageTextBox.val()
+    }, function(){
+        messageTextBox.val('');
     });
 });
 
@@ -47,13 +49,19 @@ locationButton.on('click', function (){
     if (!navigator.geolocation){
         return alert('Geolocation not supported by browser');
     }
+
+    locationButton.attr('disabled', "disabled").text('Sending location...');
+
+
     navigator.geolocation.getCurrentPosition( function(position) {
         console.log(position);
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function (){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('unable to fetch location');
     });
 });
